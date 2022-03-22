23ab2d41693a85d73d1c7fe424e331d362b63edf
Angelo Francisco <angelo.francisco@trackcash.com.br>
2022-03-22 10:36:26 -0300

    Add/ Busca parcial

--- src/util.ts
+++ src/util.ts
@@ -1,81 +1,107 @@
-const appearanceFilter = ["gender", "race", "height", "eyeColor", "hairColor"]
-const biographyFilter = ["fullName", "alterEgos", "aliases", "placeOfBirth", "firstAppearance", "publisher", "alignment"]
-const workFilter = ["occupation", "base"]
-const connectionsFilter = ["groupAffiliation", "relatives"]
+const appearanceFilter = ["gender", "race", "height", "eyeColor", "hairColor"];
+const biographyFilter = [
+  "fullName",
+  "alterEgos",
+  "aliases",
+  "placeOfBirth",
+  "firstAppearance",
+  "publisher",
+  "alignment",
+];
+const workFilter = ["occupation", "base"];
+const connectionsFilter = ["groupAffiliation", "relatives"];
 
 function search(data: any[], args: String, filter: any): any {
   const filters: any = {
     name: () => {
-      const name = data.filter((hero: any) => hero.name === args);
+      const name = data.filter((hero: any) => {
+        return hero.name.toLowerCase().includes(args.toLowerCase());
+      });
       return name;
     },
 
     appearance: () => {
-        let appearance: Object = {}
-        appearanceFilter.forEach((filter: any) => {
-            const result = data.filter((hero: any) => hero.appearance[filter] === args);
-            appearance = {...appearance, ...result}
-        })
-        return Object.values(appearance)
+      let appearance: Object = {};
+
+      appearanceFilter.forEach((filter: any) => {
+        const result = data.filter((hero: any) => {
+
+          return hero.appearance[filter] || ""
+            .toLowerCase()
+            .includes(args.toLowerCase());
+        });
+
+        appearance = { ...appearance, ...result };
+      });
+      return Object.values(appearance);
     },
 
     biography: () => {
-      let biography: Object = {}
+      let biography: Object = {};
+
       biographyFilter.forEach((filter: any) => {
-        const result = data.filter( (hero:any ) => hero.biography[filter] === args);
-        biography = {...biography, ...result}
-      })
-      return Object.values(biography)
+
+        const result = data.filter((hero: any) => {
+
+          return hero.biography[filter] || ""
+            .toLowerCase()
+            .includes(args.toLowerCase());
+        });
+
+        biography = { ...biography, ...result };
+      });
+      return Object.values(biography);
     },
 
     work: () => {
-      let work: Object = {}
+      let work: Object = {};
       workFilter.forEach((filter: any) => {
-        const result = data.filter( (hero:any ) => hero.work[filter] === args);
-        work = {...work, ...result}
-      })
-      return Object.values(work)
+        const result = data.filter((hero: any) => {
+          return hero.work[filter].toLowerCase().includes(args.toLowerCase());
+        });
+        work = { ...work, ...result };
+      });
+      return Object.values(work);
     },
 
     connections: () => {
-      let connections: Object = {}
+      let connections: Object = {};
       connectionsFilter.forEach((filter: any) => {
-        const result = data.filter( (hero:any ) => hero.connections[filter] === args);
-        connections = {...connections, ...result}
-      })
-      return Object.values(connections)
-    }
-  } 
+        const result = data.filter((hero: any) => {
+          hero.connections[filter].toLowerCase().includes(args.toLowerCase());
+        });
+        connections = { ...connections, ...result };
+      });
+      return Object.values(connections);
+    },
+  };
 
-    if(filters[filter] !== undefined){
-        return filters[filter]()
-    }
+  if (filters[filter] !== undefined) {
+    return filters[filter]();
+  }
 
-    return null
+  return null;
 }
 
-function orderFilter(x:any, y:any, order: any): any{
-
+function orderFilter(x: any, y: any, order: any): any {
   const filters: any = {
     appearance: appearanceFilter,
     biography: biographyFilter,
     work: workFilter,
-    connections: connectionsFilter
-  }
+    connections: connectionsFilter,
+  };
 
-  for(let filter in filters){
-
-    if(filters[filter].includes(order)){
-      if(x.work[order] > y.work[order]){
+  for (let filter in filters) {
+    if (filters[filter].includes(order)) {
+      if (x.work[order] > y.work[order]) {
         return 1;
       }
-      if(x.work[order] < y.work[order]){
-          return -1;
+      if (x.work[order] < y.work[order]) {
+        return -1;
       }
       return 0;
     }
-    
   }
 }
 
-export {search, orderFilter};
+export { search, orderFilter };
